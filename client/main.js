bb5ccff3ed19294c887b4d76c8289343db9c8b79
alien-droid <sharma.aditya2796@gmail.com>
2024-04-09 22:39:14 -0500

    Updated Server URL

--- client/main.js
+++ client/main.js
@@ -65,7 +65,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   // server call
-  const response = await fetch('http://localhost:5000/', {
+  const response = await fetch('https://gpt-clone-codita.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
